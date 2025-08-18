a37fee64c2aea88ff588e836d9f57114f6af1a04
Luis Monteiro <luismonteirodeveloper@gmail.com>
2025-08-18 20:14:17 -0300

    feat: Função auxiliar para retornar o valor da nota com o tipo `float`

--- src/exercicio1.java
+++ src/exercicio1.java
@@ -1,7 +1,19 @@
 public class exercicio1 {
+    // Definindo o metódo para retornar uma nota float
+    public static float retornarNotaFloat(String nota) {
+        // Tranformando a string em float
+        float notaFloat = Float.parseFloat(nota);
+        // Validando a nota
+        if (notaFloat < 0 || notaFloat > 10) {
+            System.out.println("Nota1 inválida");
+        }
+        // Retornando a nota
+        return notaFloat;
+    }
+
     public static void main(String[] args) {
         // Definindo as variáveis de notas dos alunos
-        Float media_aluno = 0.0f;
+        Float mediaAluno = 0.0f;
         Float nota1 = 0.0f;
         Float nota2 = 0.0f;
         Float nota3 = 0.0f;
@@ -12,74 +24,36 @@ public class exercicio1 {
 
         while (rodarNovamente){
             System.out.println("Digite a sua primeira nota: ");
-            nota1 = Float.parseFloat(System.console().readLine());
-
-            if (nota1 < 0 || nota1 > 10) {
-                System.out.println("Nota1 inválida");
-            }
+            nota1 = retornarNotaFloat(System.console().readLine());
 
             System.out.println("Digite a sua segunda nota: ");
-            nota2 = Float.parseFloat(System.console().readLine());
-
-            if (nota2 < 0 || nota2 > 10) {
-                System.out.println("Nota2 inválida");
-            }
+            nota2 = retornarNotaFloat(System.console().readLine());
 
             System.out.println("Digite a sua terceira nota: ");
-            nota3 = Float.parseFloat(System.console().readLine());
-
-            if (nota3 < 0 || nota3 > 10) {
-                System.out.println("Nota3 inválida");
-            }
+            nota3 = retornarNotaFloat(System.console().readLine());
 
             System.out.println("Digite a sua quarta nota: ");
-            nota4 = Float.parseFloat(System.console().readLine());
+            nota4 = retornarNotaFloat(System.console().readLine());
 
-            if (nota4 < 0 || nota4 > 10) {
-                System.out.println("Nota4 inválida");
-            }
+            mediaAluno = (nota1 + nota2 + nota3 + nota4) / 4;
 
-            media_aluno = (nota1 + nota2 + nota3 + nota4) / 4;
-
-            if (media_aluno > MEDIA) {
-                System.out.println("Aluno aprovado, média " + media_aluno);
-                System.out.println("Deseja calcular outra média? (S/N)");
-
-                String resposta = System.console().readLine();
-                if (
-                    resposta.equals("S") || resposta.equals("s") 
-                    || resposta.equals("Sim") || resposta.equals("sim")
-                ) {
-                    continue;
-                }
-                break;
-
-            } else if (media_aluno == MEDIA) {
-                System.out.println("Aluno passou na média " + media_aluno);
-
-                System.out.println("Deseja calcular outra média? (S/N)");
-                String resposta = System.console().readLine();
-                if (
-                    resposta.equals("S") || resposta.equals("s") 
-                    || resposta.equals("Sim") || resposta.equals("sim")
-                ) {
-                    continue;
-                }
-                break;
-                
+            if (mediaAluno > MEDIA) {
+                System.out.println("Aluno aprovado, média " + mediaAluno);
+            } else if (mediaAluno == MEDIA) {
+                System.out.println("Aluno passou na média " + mediaAluno);
             } else {
-                System.out.println("Aluno reprovado, média " + media_aluno);
-
-                System.out.println("Deseja calcular outra média? (S/N)");
-                String resposta = System.console().readLine();
-                if (
-                    resposta.equals("S") || resposta.equals("s") 
-                    || resposta.equals("Sim") || resposta.equals("sim")
-                ) {
-                    continue;
-                }
-                break;
+                System.out.println("Aluno reprovado, média " + mediaAluno);
             }
-        }
+            System.out.println("Deseja calcular outra média? (S/N)");
+
+            String resposta = System.console().readLine();
+            if (
+                resposta.equals("S") || resposta.equals("s") 
+                || resposta.equals("Sim") || resposta.equals("sim")
+            ) {
+                continue;
+            }
+            break;
+    }
     }
 }
