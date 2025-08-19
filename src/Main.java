48924fabcf97613569508321e87a12bfd6d2bd0a
Luis Monteiro <luismonteirodeveloper@gmail.com>
2025-08-19 17:06:03 -0300

    Exercicio de loop com foreach do JAVA

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-       exercicio3Objetos computador = new exercicio3Objetos("Acer", "Nitro 5", "Linux", 5000);
+       exercicio3Objetos computador = new exercicio3Objetos(
+        "Acer", 
+        "Nitro 5", 
+        "Linux", 
+        5000
+    );
        computador.ligar();
        computador.desligar();
        System.out.println(computador.getComputador());
--- src/exercicio2Arrays.java
+++ src/exercicio2Arrays.java
@@ -1,15 +1,42 @@
 // lidando com arrays
+import java.util.ArrayList;
+import java.util.List;
 
 public class exercicio2Arrays {
     public static void main(String[] args) {
 
         String[] nomes = new String[5];
         String[] animais = {"Cachorro", "gato", "passaros"};
+        Object[] arrayObjeto = {1, "oi", true};
+        List<Object> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add("Luis");
+        arrayList.add(20.0);
+        arrayList.add(true);
 
         System.out.println(nomes[0]);
 
+        System.out.println("For normal do Java");
         for (int i = 0; i < animais.length; i++) {
             System.out.println(animais[i]);
         }
+
+        // --------------------------------------------------
+
+        System.out.println("Foreach do Java");
+        for (Object objeto : animais) {
+            System.out.println(objeto);
+        }
+        System.out.println(arrayList);
+        for (Object objeto : arrayList) {
+            System.out.println(objeto);
+        }
+        
+        for (Object objeto : arrayObjeto) {
+            System.out.println(objeto);
+        }
+        // --------------------------------------------------
+        
+        
     }
 }
